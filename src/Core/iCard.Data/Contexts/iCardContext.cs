682cf38ed998c9e90e4ea7b4ba6e2652ddd39791
StefanKehayov <stefan.kehayov.96@gmail.com>
2021-05-12 01:04:14 +0300

    Migrated to Postgres

--- src/Core/iCard.Data/Contexts/iCardContext.cs
+++ src/Core/iCard.Data/Contexts/iCardContext.cs
@@ -1,12 +1,10 @@
-
-
 using iCard.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace iCard.Data.Contexts
 {
-    public class ICardContext: DbContext
+    public class ICardContext : DbContext
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Plan> Plan { get; set; }
@@ -15,17 +13,20 @@ namespace iCard.Data.Contexts
         public DbSet<VirtualCard> VirtualCards { get; set; }
         public DbSet<TransactionHistory> TransactionHistories { get; set; }
 
+        /*
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=localhost;Database=BalanceProjectDB;User Id=applogin;Password=12344321;");
 
-
         }
+         */
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=localhost:5432;Database=icardcloneapp;Username=applogin;Password=1234");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
-            
             modelBuilder.Entity<VirtualCard>().ToTable("VirtualCard");
 
             modelBuilder.Entity<TransactionHistory>().ToTable("TransactionHistory");
@@ -33,43 +34,31 @@ namespace iCard.Data.Contexts
             modelBuilder.Entity<Settings>().ToTable("Settings");
 
             modelBuilder.Entity<Plan>().ToTable("Plan");
-            
+
             modelBuilder.Entity<Account>().ToTable("Account");
-            
+
 
             modelBuilder.Entity<User>().ToTable("User")
                 .HasOne(user => user.Account);
-          
-            
+
 
             modelBuilder.Entity<Account>().HasMany<VirtualCard>(acc => acc.VirtualCards)
                 .WithOne(vc => vc.Account)
                 .HasForeignKey(vc => vc.AccountId);
-                      
-            
+
+
             modelBuilder.Entity<Account>()
                 .HasMany<TransactionHistory>(acc => acc.Transactions)
                 .WithOne(th => th.Account)
-                .HasForeignKey( th => th.AccountId);
-          
+                .HasForeignKey(th => th.AccountId);
 
-            
-            
+
             modelBuilder.Entity<Account>()
                 .HasOne<Settings>(acc => acc.Settings);
-        
-            
+
+
             modelBuilder.Entity<Account>()
                 .HasOne<Plan>(acc => acc.Plan);
-          
-
-
-        
-
-
         }
-
-
     }
 }
-
---
+++ src/Core/iCard.Data/Migrations/20210511220141_InitialCreate.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace iCard.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plan",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Cost = table.Column<double>(type: "double precision", nullable: false),
+                    Currency = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: false),
+                    MaxExchange = table.Column<double>(type: "double precision", nullable: false),
+                    Type = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    CardTransactions = table.Column<int>(type: "integer", nullable: false),
+                    Insurance = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plan", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AppTheme = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    GamblingBlock = table.Column<bool>(type: "boolean", nullable: false),
+                    AppNotifications = table.Column<bool>(type: "boolean", nullable: false),
+                    AutoUpdate = table.Column<bool>(type: "boolean", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Balance = table.Column<double>(type: "double precision", nullable: false),
+                    Currency = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false),
+                    PlanId = table.Column<int>(type: "integer", nullable: false),
+                    SettingsId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Account_Plan_PlanId",
+                        column: x => x.PlanId,
+                        principalTable: "Plan",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Account_Settings_SettingsId",
+                        column: x => x.SettingsId,
+                        principalTable: "Settings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TransactionCost = table.Column<double>(type: "double precision", nullable: false),
+                    Currency = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: false),
+                    TransactionTarget = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    TransactionType = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    TransactionStatus = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    Name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    AccountId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TransactionHistory_Account_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false),
+                    AccountId = table.Column<int>(type: "integer", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_User_Account_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VirtualCard",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Balance = table.Column<double>(type: "double precision", nullable: false),
+                    Currency = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: false),
+                    CardNumber = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ValidDate = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: false),
+                    CVV = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    Name = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    AccountId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VirtualCard", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VirtualCard_Account_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Account",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_PlanId",
+                table: "Account",
+                column: "PlanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Account_SettingsId",
+                table: "Account",
+                column: "SettingsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionHistory_AccountId",
+                table: "TransactionHistory",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_AccountId",
+                table: "User",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VirtualCard_AccountId",
+                table: "VirtualCard",
+                column: "AccountId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransactionHistory");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "VirtualCard");
+
+            migrationBuilder.DropTable(
+                name: "Account");
+
+            migrationBuilder.DropTable(
+                name: "Plan");
+
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
